3b83e8f116f33dcb792eccba4978579d19f6df46
Sarulian <tedjamesrobinson@gmail.com>
2017-04-04 21:22:32 -0700

    cute stuff

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return 'Hey cutie pie'
 
+@app.route('/rachael')
+def rachael():
+    return 'I love you!'
+
+@app.route('/daisy')
+def cakes():
+    return 'I love you more ;)'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
