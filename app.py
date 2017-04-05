52848d596ac9b8aaf8698e81a4bae1b7b14c8b4f
Sarulian <tedjamesrobinson@gmail.com>
2017-04-04 21:44:27 -0700

    Adding first html, css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def rachael():
 @app.route('/daisy')
 def cakes():
     return 'I love you more ;)'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
