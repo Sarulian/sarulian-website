eae10d13f44114ca73df03aca76f54ea3231342b
Sarulian <tedjamesrobinson@gmail.com>
2017-04-07 07:00:57 -0700

    changed app.py to point to page.html

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/hello/<name>')
+@app.route('/css')
 def hello(name):
-    return render_template('page.html', name=name)
+    return render_template('page.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4444)
