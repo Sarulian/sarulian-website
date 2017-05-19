475f668e25345e60a72b20e02cd40315bcd5b65c
Sarulian <tedjamesrobinson@gmail.com>
2017-05-19 15:17:46 -0700

    initial shopper commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import buzzfeedshopper
 
 app = Flask(__name__)
 
@@ -6,17 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/css.html')
-def css():
-    return render_template('css.html')
+@app.route('/recipe/<buzzfeedlink>')
+def recipe(buzzfeedlink):
+	return 'Creating list for %s' % buzzfeedlink
 
-@app.route('/dummy.html')
-def dummy():
-    return render_template('dummy.html')
-
-@app.route('/boxes.html')
-def boxes():
-	return render_template('boxes.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4444)
